8a850fd4d95932d9699bbf091655dbb5fb3729b5
Chathuri Samarasekara <73504291+chathuri-sam@users.noreply.github.com>
2024-12-02 18:06:56 +1100

    Update Reproducible Swamp Data Analysis.R

--- Reproducible Swamp Data Analysis.R
+++ Reproducible Swamp Data Analysis.R
@@ -1,19 +1,3 @@
-remove(list = ls())
-library(devtools)
-
-# Install RandomFields which is not on CRAN anymore
-devtools::install_version("RandomFieldsUtils", dependencies = TRUE, version = "1.2.5", repos = "http://cran.us.r-project.org")
-devtools::install_version("RandomFields", dependencies = TRUE, version = "3.3.14", repos = "http://cran.us.r-project.org")
-
-# Old version of geostatsp that works with old Matrix package
-devtools::install_version("geostatsp", dependencies = TRUE, version = "2.0.0", repos = "http://cran.us.r-project.org")
-
-# Install Multilogreg package
-devtools::install_github("IbTJensen/Multilogreg")
-
-# Install ppjsdm package
-devtools::install_github("iflint1/ppjsdm")
-
 library(ecespa)
 library(ggplot2)
 library(Multilogreg)
